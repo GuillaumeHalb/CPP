4eb1f2ae6b9bdc40e6068c7553e1416764eb3303
Filipe Gomes <filipe.gomes@ensimag.grenoble-inp.fr>
2016-02-11 12:45:15 +0100

    tests opérateur +

--- TP2_HALBG_GOMESF/src/Main.cpp
+++ TP2_HALBG_GOMESF/src/Main.cpp
@@ -14,5 +14,16 @@ int main() {
   const Dvector v2(1);
   double val = v2(0);
   std::cout << val<<std::endl;
+  
+  /* Test opérateurs binaires */
+  Dvector vect1(3, 6);
+  Dvector vect2(3, 2);
+  Dvector res(3);
+  double scal = 2.0;
+  
+  std::cout << "test de: + "<<std::endl;
+  vect1.display(std::cout);
+  res = vect1 + scal;
+  res.display(std::cout);
 }
 
