cb6e5495443ddfc0e7823a1f38a2c9820a67cbd4
Filipe Gomes <filipe.gomes@ensimag.grenoble-inp.fr>
2016-03-24 11:49:10 +0100

    création classe maillage

---
+++ TP4_HALBG_GOMESF/src/Maillage.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   Maillage.h
+ * Author: gomesf
+ *
+ * Created on 24 mars 2016, 11:48
+ */
+
+#ifndef MAILLAGE_H
+#define	MAILLAGE_H
+
+class Maillage {
+public:
+    Maillage();
+    Maillage(const Maillage& orig);
+    virtual ~Maillage();
+private:
+
+};
+
+#endif	/* MAILLAGE_H */
+
---
+++ TP4_HALBG_GOMESF/src/Maillage.tpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Maillage.tpp
+ * Author: gomesf
+ * 
+ * Created on 24 mars 2016, 11:48
+ */
+
+#include "Maillage.h"
+
+Maillage::Maillage() {
+}
+
+Maillage::Maillage(const Maillage& orig) {
+}
+
+Maillage::~Maillage() {
+}
+
